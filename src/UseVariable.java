8f41cf4a9fa1c637029b6f8369cc985895cdabf7
Shibazaki Takahiro <Shibazaki Takahiro@LAPTOP-5BU4T3JV>
2023-01-29 18:23:18 +0900

    Javaプログラミング入門学習終了

---
+++ src/UseVariable.java
@@ -0,0 +1,14 @@
+
+public class UseVariable {
+
+    public static void main(String[] args) {
+        int number2;
+        String str2;
+        number2 = 100;
+        str2 = "xyz";
+        System.out.println(number2);
+        System.out.println(str2);
+
+    }
+
+}
